efe8207b4b2b9adc2715d705aa5248aa5064de2b
Ahneb <benjaminha1996@gmail.com>
2022-10-31 16:48:58 -0700

    Added welcome page info cards. added css for welcome page info cards. added script for swiper cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1003,3 +1003,10 @@ const setUserInfo = function(){
   localStorage.setItem('userPreferances',JSON.stringify(dietsPreferences));
 }
 */
+
+var swiper = new Swiper(".mySwiper", {
+	pagination: {
+		el: ".swiper-pagination",
+		dynamicBullets: true,
+	},
+});
